bdebda9319a8709651759873a3132e56f7021cba
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-16 22:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const arr = Array.form(str);
+	for(let i=0;arr.length;i++)
+		{
+			let count =0;
+			for(let j=0;j<arr.length;j++)
+				{
+					if(arr[i]===arr[j])
+						
+					{
+						count++;
+					}
+				}
+			if(count===1)
+			{
+				return arr[i];
+			}
+		}
+	return null;	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
